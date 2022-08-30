330f708dd67e83a237ad962580782155aa026450
say_no <dallas2112@yandex.ru>
2022-08-31 01:33:08 +0300

    feat: popup opening

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const popup = document.querySelector('.header__popup');
+const menuOpen = document.querySelector('.header__menu-image');
+const menuClose = document.querySelector('.header__popup-image');
+
+menuOpen.addEventListener('click', function(e) {
+  e.preventDefault;
+  popup.classList.add('header__popup_type_opened');
+})
+
+menuClose.addEventListener('click', function(e) {
+  e.preventDefault;
+  popup.classList.remove('header__popup_type_opened');
+})
